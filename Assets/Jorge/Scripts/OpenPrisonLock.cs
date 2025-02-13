99997eb88da047b19124df0ba4f01c9d98e6689d
Jorge Casas <casasguerrerojorge@gmail.com>
2025-02-13 18:02:51 +0100

    Sala celda y pasillo 1

---
+++ Assets/Jorge/Scripts/OpenPrisonLock.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPrisonLock : MonoBehaviour
+{
+    private bool _doorOpen = false;
+    private Animator _animator;
+    [SerializeField] private GameObject _door;  // Go que se abrirá al introducir la llave
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = _door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(!_doorOpen && other.transform.gameObject.layer == 11)
+        {
+            _animator.SetTrigger("TrOpen");
+            _doorOpen = true;
+        }
+    }
+}
