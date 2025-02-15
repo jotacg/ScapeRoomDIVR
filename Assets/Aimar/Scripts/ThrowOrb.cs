66d50abaec04ac71ebbdc74317d0524a615d46b5
AimJim <92112507+AimJim@users.noreply.github.com>
2025-02-15 19:55:23 +0100

    fix orbe

--- Assets/Aimar/Scripts/ThrowOrb.cs
+++ Assets/Aimar/Scripts/ThrowOrb.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class ThrowOrb : MonoBehaviour
 {
-    Vector3 dir;
-    Vector3 prevDir;
-
-    [SerializeField]
-    float launchForce;
 
     Rigidbody rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
-    private void Update()
-    {
-        dir = prevDir - transform.position;
-        prevDir = transform.position;
-    }
+   
 
    
     public void OnSelectExit()
     {
         rb.constraints = RigidbodyConstraints.None;
-        rb.AddForce(dir * launchForce);
+        
     }
 
     private void OnCollisionEnter(Collision collision)
