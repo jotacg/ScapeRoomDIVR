e42d7bb3d713e48bbd73bff2d874d13c3bbe68a3
German Viscuso <germanviscuso@gmail.com>
2025-02-25 07:19:04 +0100

    Secuencia de gemas reparada

--- Assets/German/Scripts/ActivateOnFirstGem.cs
+++ Assets/German/Scripts/ActivateOnFirstGem.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class ActivateOnFirstGem : MonoBehaviour
 {
-    public AudioSource backgroundAudio; // Assign background loop (stops when activated)
-    public AudioSource activationAudio; // Assign activation sound (plays when activated)
-    public bool deactivateOnStart = true; // Editable in Unity Inspector
+    public AudioSource backgroundAudio;  // Assign background loop (stops when activated)
+    public AudioSource activationAudio;  // Assign activation sound (plays when activated)
+    public bool deactivateOnStart = true;  // Editable in Unity Inspector
+
+    private Renderer[] renderers;  // We'll store all renderers so we can enable/disable them
 
     private void OnEnable()
     {
@@ -18,16 +20,28 @@ public class ActivateOnFirstGem : MonoBehaviour
 
     private void Start()
     {
+        // Gather all Renderer components on this object and its children
+        renderers = GetComponentsInChildren<Renderer>();
+
+        // If the user wants to "disable" the object at start, just hide its renderers
         if (deactivateOnStart)
         {
-            gameObject.SetActive(false); // Start disabled if set in editor
+            foreach (Renderer r in renderers)
+            {
+                r.enabled = false;
+            }
         }
     }
 
     private void ActivateObject()
     {
         Debug.Log("✨ First Gem inserted! Activating object: " + gameObject.name);
-        gameObject.SetActive(true);
+
+        // Re-enable the renderers, so the object becomes visible again
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = true;
+        }
 
         if (backgroundAudio != null)
         {
--- Assets/German/Scripts/ActivateOnSecondGem.cs
+++ Assets/German/Scripts/ActivateOnSecondGem.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class ActivateOnSecondGem : MonoBehaviour
 {
-    public AudioSource backgroundAudio;
-    public AudioSource activationAudio;
-    public bool deactivateOnStart = true; // Editable in Unity Inspector
+    public AudioSource backgroundAudio;  // Assign background loop (stops when activated)
+    public AudioSource activationAudio;  // Assign activation sound (plays when activated)
+    public bool deactivateOnStart = true;  // Editable in Unity Inspector
+
+    private Renderer[] renderers;  // We'll store all renderers to enable/disable them
 
     private void OnEnable()
     {
@@ -18,25 +20,37 @@ public class ActivateOnSecondGem : MonoBehaviour
 
     private void Start()
     {
+        // Gather all Renderer components on this object (and children)
+        renderers = GetComponentsInChildren<Renderer>();
+
+        // If the user wants to "disable" the object at start, just hide its renderers
         if (deactivateOnStart)
         {
-            gameObject.SetActive(false);
+            foreach (Renderer r in renderers)
+            {
+                r.enabled = false;
+            }
         }
     }
 
     private void ActivateObject()
     {
         Debug.Log("🔆 Second Gem inserted! Activating object: " + gameObject.name);
-        gameObject.SetActive(true);
+
+        // Re-enable the renderers, so the object becomes visible again
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = true;
+        }
 
         if (backgroundAudio != null)
         {
-            backgroundAudio.Stop();
+            backgroundAudio.Stop(); // Stop background audio permanently
         }
 
         if (activationAudio != null)
         {
-            activationAudio.Play();
+            activationAudio.Play(); // Play activation sound effect
         }
     }
 }
